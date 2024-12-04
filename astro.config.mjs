d9aaa35d2ff146158866dc60b9eb96d2097dba58
DROO <andrewtehsailor@gmail.com>
2024-12-04 13:25:59 +0100

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 
 export default defineConfig({
-  site: 'https://www.droo.foo', // www.DROO.FOO
+  site: 'https://www.droo.foo',
   integrations: [],
+  adapter: cloudflare(),
 });

fae33415f4e3e7ceb51475ac1eb6c04f4cd811d3
hydepwns <andretehsailor@gmail.com>
2025-07-08 15:36:07 +0200

    Update build configuration and dependencies
    - Simplify test task in mix.exs to remove assets build dependency
    - Update libsignal-protocol-nif dependency with latest commits
    - Improve build process efficiency

--- mix.exs
+++ mix.exs
@@ -162,7 +162,7 @@ defmodule HydepwnsLiveview.MixProject do
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["assets.build", "test"],
+      test: ["test"],
       "assets.setup": ["esbuild.install --if-missing", "sass.install --if-missing"],
       "assets.build": ["esbuild hydepwns_liveview", "sass default"],
       "assets.deploy": [
